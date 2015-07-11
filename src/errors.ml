76198d5903f8133dc36c88b560c5535a7e05e8fd
fubuloubu <fubuloubu@gmail.com>
2015-07-11 13:08:54 -0400

    Reformatted how errors are processed for Parsing error to be correct

--- src/errors.ml
+++ src/errors.ml
@@ -1,31 +1,34 @@
+open Lexing
+open Parsing
+
 (* Define errors *)
 let issue msg start finish  = 
         Printf.sprintf "(line %d: char %d..%d): %s" 
-                (start.Lexing.pos_lnum)
-                (start.Lexing.pos_cnum - start.Lexing.pos_bol) 
-                (finish.Lexing.pos_cnum - finish.Lexing.pos_bol)
+                (start.pos_lnum)
+                (start.pos_cnum - start.pos_bol) 
+                (finish.pos_cnum - finish.pos_bol)
                 msg
 exception XML_Error of string
 let xml_error lexbuf = raise
                 (XML_Error
                     (issue 
-                        ("Badly Formatted XML")
-                        (lexbuf.Lexing.lex_start_p) 
-                        (lexbuf.Lexing.lex_curr_p)
+                        ("Badly Formatted XML: " ^ (Lexing.lexeme lexbuf))
+                        (Lexing.lexeme_start_p lexbuf) 
+                        (Lexing.lexeme_end_p lexbuf)
                     )
                 )
 let xml_warning lexbuf = ignore 
                     (issue 
-                        ("Warning -- Skipping XML")
-                        (lexbuf.Lexing.lex_start_p) 
-                        (lexbuf.Lexing.lex_curr_p)
+                        ("Warning -- Skipping XML: " ^ (Lexing.lexeme lexbuf))
+                        (Lexing.lexeme_start_p lexbuf) 
+                        (Lexing.lexeme_end_p lexbuf)
                     )
 exception XML_Parse_Error of string
-let xml_parse_error lexbuf = raise
+let xml_parse_error nterm = raise
                 (XML_Parse_Error
                     (issue 
                         ("Badly Formatted XML")
-                        (lexbuf.Lexing.lex_start_p) 
-                        (lexbuf.Lexing.lex_curr_p)
+                        (rhs_start_pos nterm) 
+                        (rhs_end_pos nterm)
                     )
                 )
