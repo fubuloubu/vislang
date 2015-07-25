bf0fc6562df4219748ef085f1628e1ad5db8fb88
fubuloubu <fubuloubu@gmail.com>
2015-07-24 20:17:28 -0400

    Removed interpreter

--- src/vislang.ml
+++ src/vislang.ml
@@ -1,13 +1,14 @@
 open Scanner
 open Parser
 open Blockify
+open Compile
+open Bytecode
 
 type action = Ast | Interpret | Bytecode | Compile
 
 let _ =
     let action = if Array.length Sys.argv > 1 then
         List.assoc Sys.argv.(1) [ ("-a", Ast);
-                                  ("-i", Interpret);
                                   ("-b", Bytecode);
                                   ("-c", Compile) ]
     else Compile in
@@ -19,8 +20,7 @@ let _ =
     match action with
           Ast       -> let listing = Ast.string_of_xml xml_tree
                         in print_string listing
-        | Interpret -> ()(*ignore (Interpret.run program) *)
-        | Bytecode  -> ()(*let listing = Bytecode.string_of_prog
+        | Bytecode  -> let listing = Bytecode.string_of_prog
                                     (Compile.translate program)
-                        in print_endline listing *)
-        | Compile   -> ()(*Execute.execute_prog (Compile.translate program) *)
+                        in print_endline listing
+        | Compile   -> print_endline (Compile.translate program)
