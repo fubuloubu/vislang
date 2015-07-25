04113d777fc7f59741a3d5eb9300466f2add2d5d
fubuloubu <fubuloubu@gmail.com>
2015-07-25 16:29:16 -0400

    Still working on this one. does notgenerate mli file or compile

--- src/blockify.ml
+++ src/blockify.ml
@@ -1,5 +1,4 @@
 open Xst
-open Bot
 open Errors
 
 let get_attr attribute xml_obj = 
@@ -9,10 +8,19 @@ let get_attr attribute xml_obj =
           | [a]     -> a.avalue
           | _ :: _  -> object_error ("Too many attributes named " ^ attribute)
 
+let get_connection input_to xml_obj =
+    let input_from = List.filter (fun x -> (get_attr "to" x) = input_to)
+        (List.filter (fun x -> x.tagname = "CONNECTION") xml_obj.inner_objs) in
+        match input_from with
+            []      -> object_error ("No connection found for " ^ input_to )
+          | [from]  -> from
+          | _ :: _  -> object_error ("Too many connections defined for " ^ input_to )
+
 let blockify xml_obj = 
     match xml_obj.tagname with
-          "BLOCK"   -> new block xml_obj
-        (*| "INPUT"   -> new input    xml_obj
+          "BLOCK"   -> new block    xml_obj
+        | "INPUT"   -> new input    xml_obj
         | "OUTPUT"  -> new output   xml_obj
-        *)(* CONNECTION blocks are not supported by this operation *)
+        (* CONNECTION blocks are not supported by this operation. 
+         * See get_connection above *)
         | _ as name     -> object_error ("Tag " ^ name ^ " not supported.")
