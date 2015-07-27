190d75fb9f0395268740100f3d1523175e0d3fb7
fubuloubu <fubuloubu@gmail.com>
2015-07-27 19:08:41 -0400

    Simplified blockification process. Moving the trace function to bytecode production

--- src/blockify.ml
+++ src/blockify.ml
@@ -8,6 +8,12 @@ let get_attr attribute xml_obj =
           | [a]     -> a.avalue
           | _ :: _  -> object_error ("Too many attributes named " ^ attribute)
 
+let get_num_connection xml_obj =
+    let inputs = List.filter
+                 (fun x -> x.tagname = "CONNECTION")
+                 xml_obj.inner_objs in
+        List.length inputs
+
 let get_connection input_to xml_obj =
     let input_from = List.filter (fun x -> (get_attr "to" x) = input_to)
         (List.filter (fun x -> x.tagname = "CONNECTION") xml_obj.inner_objs) in
@@ -20,15 +26,16 @@ let get_connection input_to xml_obj =
 class virtual base xml_obj = object
     val name = Xst.string_of_value (get_attr "name" xml_obj)
     method name = name
+    method virtual get_inputs   : string list
     method virtual print_class  : string
     method virtual bytecode     : string
     method virtual print_obj    : string
 end;;
 
 (* Block class: inherits from base, is a container for other blocks *)
-class block block_trace xml_obj = object (self)
+class block blockify xml_obj = object (self)
     inherit base xml_obj as super
-    val inner_objs = block_trace xml_obj.inner_objs
+    val inner_objs = List.map blockify xml_obj.inner_objs
     method get_inputs =
         List.map    (fun (x : base) -> (x :> base) #name)
         (List.filter (fun (x : base) -> ((x :> base) #print_class) = "input")
@@ -62,17 +69,19 @@ end;;
 (* Input class: *)
 class input xml_obj = object (self)
     inherit io_part xml_obj as super
-    method print_class= "input"
-    method bytecode  =  "bytecode for input" ^ name ^ "\n"
-    method print_obj  = "\"input\": { " ^
-                        "\"name\":\"" ^ name ^ "\", " ^
-                        "\"scope\":" ^ scope ^ "\", " ^
-                        "\"size\":" ^ Xst.string_of_value (size) ^ "\" }"
+    method get_inputs   = []
+    method print_class  = "input"
+    method bytecode     =  "bytecode for input" ^ name ^ "\n"
+    method print_obj    = "\"input\": { " ^
+                          "\"name\":\"" ^ name ^ "\", " ^
+                          "\"scope\":" ^ scope ^ "\", " ^
+                          "\"size\":" ^ Xst.string_of_value (size) ^ "\" }"
 end;;
 
 (* Output class: *)
 class output xml_obj = object (self)
     inherit io_part xml_obj as super
+    method get_inputs   = [(*FIXME: get_connection name xml_obj*)]
     method print_class = "output"
     method bytecode  =  "bytecode for input" ^ name ^ "\n"
     method print_obj  = "\"output\": { " ^
@@ -85,7 +94,7 @@ end;;
 (* Blockify goes through and matches the tagname to the appropiate object *)
 let rec blockify xml_obj = 
     match xml_obj.tagname with
-          "BLOCK"   -> (new block block_trace xml_obj :> base)
+          "BLOCK"   -> (new block blockify xml_obj :> base)
           (* Note: passing blockify into block instantiation because it can't 
            * see at compile time what the function blockify is referring to *)
         | "INPUT"   -> (new input   xml_obj :> base)
@@ -93,13 +102,6 @@ let rec blockify xml_obj =
         (* CONNECTION blocks are not supported by this operation. 
          * See get_connection above *)
         | _ as name -> object_error ("Tag " ^ name ^ " not supported.")
-
-(* Block Trace intelligently traces through the objects from output to input and
- * finds an appropiate path through the program such that when it is compiled
- * in the order determined to be okay by block trace, no issues occur *)
-and block_trace obj_list =
-(* TODO: This needs to be smarter and "trace" through inner blocks *)
-    List.map blockify obj_list
 
 (* Main caller function simply to protect against top level blocks not being
  * of type BLOCK *)
