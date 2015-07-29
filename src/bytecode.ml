dbe2f4a26665432983049142e98ed0ae1d44c362
fubuloubu <fubuloubu@gmail.com>
2015-07-29 00:20:12 -0400

    Managed to get algorithm working. Currently is not outputting inputs though

--- src/bytecode.ml
+++ src/bytecode.ml
@@ -1,22 +1,10 @@
 open Blockify
+open Errors
 (* Block Parse intelligently traces through the objects inside a block from
  * output to input and finds an appropiate path through the program such
  * that when the bytecode is extracted in the order obtained here,
  * the program is consistent and no runtimes issues occur. *)
 let rec block_parse top =
-    (* TEMP: *) (top :> block) #inner_objs
-    (*let outputs  = List.filter 
-        (fun x -> x.tagname = "OUTPUT")
-        xml_list :: List.filter 
-        (fun x -> x.tagname = "MEM")
-        xml_list in
-    let rec reduce_xml_list xml_list obj_list =
-        match obj_list with
-            []       -> xml_list
-          | hd :: tl -> let xml_list = List.filter 
-                        (fun x -> (get_attr "name" x) <> (hd :> base) #name)
-                        xml_list in
-                        reduce_xml_list xml_list obj_list in
     (* Algorithm: 
      * The block trace algorithm will get the list of outputs from the
      * current block level, and recursively traverse the current object
@@ -31,33 +19,58 @@ let rec block_parse top =
      * detected are blockified and the entire list of objects is added to the
      * list of priors. This process continues until all output and memory
      * blocks successfully traverse back to inputs or priors branches. *)
-    let rec trace prior_list trace_list xml_list curr =
-        match xml_list with
+    let rec trace block_list prior_list trace_list current =
+        match ((current :> base) #print_class) with
+            "input"     -> current :: trace_list
+          | "memory"    -> current :: trace_list
+          | "const"     -> current :: trace_list
+          | "dt"        -> current :: trace_list
+          | _ as blk    -> (*FIXME: let () = Printf.printf "%s\n" blk in;*)
+          let compare_obj n = (fun x -> (x :> base) #name = n)
+           in
+            if List.exists (compare_obj current#name) trace_list
+            then object_error (blk ^ ": " ^ ((current :> base) #name) ^
+                                     " is in an algebraic loop...")
+            else if List.exists (compare_obj current#name) prior_list
+                 then current :: trace_list
+                 else let input_names = ((current :> base) #get_inputs) 
+                      and find_fun = (fun x -> List.find (compare_obj x) block_list)
+                       in let input_list = (List.map find_fun input_names)
+                       in trace_split block_list prior_list trace_list input_list
+    (* for each input of a block, trace out the list from that point on *)
+    and trace_split block_list prior_list trace_list input_list =
+        match input_list with
+            []          -> trace_list
+          | hd :: tl    -> let trace_list = trace_list @
+                               (trace block_list prior_list trace_list hd)
+                            in trace_split block_list prior_list trace_list tl
+            
+    (* trace_start function: this function is the wrapper used to call the
+     * inner trace algorithm. It recurses through the list of start objects,
+     * applying the trace algorithm for each object, then appending the result
+     * to the list of priors for the next recursion *)
+     in 
+     let rec trace_start block_list prior_list start_list =
+        match start_list with
             []       -> prior_list
-          | hd :: tl -> let curr = (blockify hd)
-                        and prior_list = prior_list :: curr in
-                        trace prior_list trace_list tl curr 
-        (*let connections = (curr :> base) #get_inputs
-        (* filter xml_list to obtain all blocks referenced by connections *)
-        let connected_blocks = *)
-        (* if numbers don't match, something went wrong *)
-        (* do a match on block type *)
-        (* if block is input, blockify and terminate*)
-        (* if block is mem, terminate*)
-        (* if block is in current trace_list, raise error*)
-        (* if block is in prior_list, terminate *)
-        (* else, blockify block and add it to trace_list
-         * then recurse on that block*)
-    (* return object trace to block_trace *)
-    in let rec output_trace outputs xml_list obj_list =
-        match outputs with 
-            []       -> obj_list
-          | hd :: tl -> let obj_list = trace obj_list [] xml_list (blockify hd) 
-                        and xml_list = reduce_xml_list xml_list obj_list
-                        in output_trace tl xml_list obj_list
+          | hd :: tl -> let prior_list = prior_list @ 
+                            (trace block_list prior_list [] hd)
+                         in trace_start block_list prior_list tl
 
-    in output_trace outputs xml_list []*)
+    (* start_list: the list of objects in the top block used to prime the trace 
+     * algorithm. All outputs and memory blocks are added to the start list
+     * because *)
+     in 
+    let objs = (top :> base) #inner_objs
+     in 
+    let start_list  = 
+       (List.filter (fun x -> (x :> base) #print_class = "output") objs) 
+     @ (List.filter (fun x -> (x :> base) #print_class = "memory") objs) 
+    
+    (* Kick off wrapper function *)
+     in trace_start objs start_list []
 
+(* Wrapper function that returns all the bytecode for the parsed block tree *)
 let parse_block_tree program = 
     String.concat "\n" (
         List.map (fun x -> (x :> base) #bytecode) 
