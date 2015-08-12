0a6832d329780bf672456f27fa91afd6c5c26e48
fubuloubu <fubuloubu@gmail.com>
2015-08-12 14:49:35 -0400

    Added ability to call vlcc with or without direct file I/O

--- src/vislang.ml
+++ src/vislang.ml
@@ -5,15 +5,22 @@ open Blockparse
 open Compile
 
 type action = BlockTree | Compile | DebugCode
+type rwfile = File | StdIO
 
 let _ =
     let action = if Array.length Sys.argv > 1 then
         List.assoc Sys.argv.(1) [ ("-b", BlockTree);
                                   ("-c", Compile); 
                                   ("-d", DebugCode)]
-    else Compile in
-    
-    let lexbuf = Lexing.from_channel stdin in
+    else Compile
+    and rwfile = if (Array.length Sys.argv > 2) then File else StdIO in
+
+    let filein =
+        match rwfile with
+            File  -> (open_in Sys.argv.(2))
+          | StdIO -> stdin
+     in
+    let lexbuf = Lexing.from_channel filein in
     let xml_tree = Xparser.xml_tree Xscanner.token lexbuf in
     let block_tree = Blockify.parse_xml_tree xml_tree in
     let program = Blockparse.block_parse block_tree in
@@ -22,4 +29,10 @@ let _ =
               BlockTree -> Blockparse.print_list program
             | Compile   -> Compile.translate program
             | DebugCode -> Compile.gen_debug_code program
-     in print_string listing
+     in match rwfile with
+            File  -> output_string 
+                        (open_out 
+                            (Str.global_replace (Str.regexp "\.vl") ".c" Sys.argv.(2))
+                        )
+                        listing
+          | StdIO -> print_string listing
